1df224281795b2f667dd3fae0ed49981aa8e2c28
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2019-05-14 15:51:04 -0700

    Add flush

--- src/main/c/com_example_NativeWrapper.c
+++ src/main/c/com_example_NativeWrapper.c
@@ -5,10 +5,11 @@
 
 JNIEXPORT jstring JNICALL Java_com_example_NativeWrapper_testNativeMethod(JNIEnv *env, jclass c, jstring s) {
   const char *nativeString = (*env)->GetStringUTFChars(env, s, 0);
-  printf("Hello World from C! %s", nativeString);
+  printf("Hello World from C! %s\n", nativeString);
+  fflush(stdout);
   if (strcmp(nativeString, "test") == 0) {
     const char *p = 0;
-    printf("Printing non-sense value: %s", p);
+    printf("Printing nonsense value: %s", p);
   }
   (*env)->ReleaseStringUTFChars(env, s, nativeString);
   return s;
