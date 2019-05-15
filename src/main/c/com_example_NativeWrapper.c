cd65e6e281eef350bcf2faca8fc9bc452c590e1f
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2019-05-15 09:13:26 -0700

    JNI debug

--- src/main/c/com_example_NativeWrapper.c
+++ src/main/c/com_example_NativeWrapper.c
@@ -8,6 +8,7 @@ JNIEXPORT jstring JNICALL Java_com_example_NativeWrapper_testNativeMethod(JNIEnv
   printf("Hello World from C: \"%s\", len: %d\n", nativeString, strlen(nativeString));
   fflush(stdout);
   printf("Comparison: %d\n", strcmp(nativeString, "test"));
+  fflush(stdout);
   if (strcmp(nativeString, "test") == 0) {
     const char *p = 0;
     printf("Printing nonsense value: %s", p);
