eee006a5515361a0cdbc72aab3788c700f6b67f7
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2019-05-20 13:05:17 -0700

    Add option to allocate a large block of native memory

--- src/main/c/com_example_NativeWrapper.c
+++ src/main/c/com_example_NativeWrapper.c
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
+#include <sys/mman.h>
 #include "com_example_NativeWrapper.h"
 
 JNIEXPORT jstring JNICALL Java_com_example_NativeWrapper_testNativeMethod(JNIEnv *env, jclass c, jstring s) {
@@ -10,6 +12,15 @@ JNIEXPORT jstring JNICALL Java_com_example_NativeWrapper_testNativeMethod(JNIEnv
   if (strcmp(nativeString, "test") == 0) {
     int *p = NULL;
     printf("Printing nonsense value: %d", *p);
+  } else if (strcmp(nativeString, "nativemem") == 0) {    
+    void *p = mmap((void*)0x10000000, 0xA0000000, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    if (p) {
+      printf("Allocated mmap %p\n", p);
+      fflush(stdout);
+    } else {
+      printf("Failed to allocate\n");
+      fflush(stdout);
+    }
   }
   (*env)->ReleaseStringUTFChars(env, s, nativeString);
   return s;
